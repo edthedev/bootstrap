9e0c573e9af563d7c5baa8585610eabb32e907c4
Edward Delaporte <edthedev@gmail.com>
2014-05-10 13:50:18 -0500

    Switched to public URL to allow git clone without SSH keys.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,14 @@
 echo "Debian/Ubuntu Bootstrap Script by EdTheDev"
 echo " This is an interactive script."
 
-echo "You need Vim and Screen to survive."
-sudo apt-get install vim git screen xclip openssh-server
+# echo "You need Vim and Screen to survive."
+# sudo apt-get install vim git screen xclip openssh-server
 
 read -n 1 -r -p "Clone the rest of this repository (recommended)? [y/N]"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    git clone git@github.com:edthedev/bootstrap.git
+    # git clone git@github.com:edthedev/bootstrap.git
+	git clone https://github.com/edthedev/bootstrap.git
 fi
 
 read -n 1 -r -p "Run the rest of the bootrap scripts now? [y/N]"
