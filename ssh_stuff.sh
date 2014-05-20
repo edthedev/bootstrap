b1979ce7ecc359fe1a3c5b690300538bc651a2f4
Edward Delaporte <edward@delaporte.us>
2014-05-20 17:22:20 -0500

    Added TextEdit to open pubkey for Mac.

--- ssh_stuff.sh
+++ ssh_stuff.sh
@@ -1,3 +1,7 @@
+# web=firefox # for Ubuntu/Fedora
+# web=iceweasel # for Debian
+web="open -a Firefox.app" # for Apple OSX Mavericks
+
 read -n 1 -r -p "Generate a new SSH key? [y/N]"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -8,12 +12,14 @@ then
 fi
 
 xclip -sel clip < ~/.ssh/id_rsa.pub
-echo "Copied your Public Key to the clipboard."
+echo "Copied your Public Key to the clipboard (Debian/Ubuntu/Fedora)."
 
 echo "Your hostname is:"
 hostname
 
 echo "Paste your new SSH key into your GitHub and BitBucket accounts."
-firefox https://github.com/settings/ssh
-firefox https://bitbucket.org/account/user/edthedev/ssh-keys/
+$web https://github.com/settings/ssh
+$web https://bitbucket.org/account/user/edthedev/ssh-keys/
 
+echo "Opening pubkey in editor (OSX Mavericks)"
+open -a TextEdit ~/.ssh/id_rsa.pub
