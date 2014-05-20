3d8ffdb9e7ef7979c50e4dab2db2d3505c48abe9
Edward Delaporte <edward@delaporte.us>
2014-05-20 17:13:32 -0500

    Cleaned up Ansible install script a bit.

--- ansible.sh
+++ ansible.sh
@@ -1,11 +1,4 @@
 cd ~
-git clone https://github.com/ansible/ansible.git
-cd ansible
-git checkout v1.6.1
-# make install --prefix=~/python
-# sudo mkdir /etc/ansible
-# sudo cp ~/ansible/examples/hosts /etc/ansible/
-cp ~/ansible/examples/hosts ~/python/
 
 # Fixes for Mavericks
 sudo easy_install --upgrade pip
@@ -14,7 +7,16 @@ sudo ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future pip 
 # For everyone
 sudo pip install PyYAML jinja2 paramiko pycrypto
 
-echo "PYTHONPATH=$PYTHONPATH:~/ansible/bin" >> ~/.bashrc
-echo "PATH=$PATH:~/ansible/bin" >> ~/.bashrc
+# Install Ansible system wide.
+git clone https://github.com/ansible/ansible.git
+cd ansible
+git checkout v1.6.1
+# make install --prefix=~/python
+make install 
+# cp ~/ansible/examples/hosts ~/python/
 
+# Setup example configuration
+sudo mkdir /etc/ansible
+sudo cp ~/ansible/examples/hosts /etc/ansible/
 
+echo "Check /etc/ansible/hosts"
