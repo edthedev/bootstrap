58184efed77ea642520703ce81b74cca06af39da
Edward Delaporte <edward@delaporte.us>
2014-05-22 18:12:06 -0500

    Added amethyst for window tiling on Mac.

--- apps-mac.sh
+++ apps-mac.sh
@@ -1,5 +1,12 @@
 web="open -a Firefox.app" # for Apple OSX Mavericks
 
+# File sync
 $web https://copy.com/install/mac/Copy.dmg 
 $web https://uofi.app.box.com/settings/sync
+
+# Window tiling
+$web https://github.com/ianyh/Amethyst
+
+# Window switching that recognizes more than one Firefox window
 $web http://bahoom.com/hyperswitch
+
