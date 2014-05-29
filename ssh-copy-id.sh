aaa2a4996caa2d31f88768671f9e01f44ef09c23
Edward Delaporte <edward@delaporte.us>
2014-05-29 13:37:37 -0500

    Updated version that deposits a copy of id_rsa.pub

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -1,3 +1,4 @@
 function ssh-copy-id {
-    cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+	cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+	cat ~/.ssh/id_rsa.pub | ssh $1 "cat >> ~/id_rsa.pub"
 }
