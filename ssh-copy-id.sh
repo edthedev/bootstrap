5dc92a711c65708a4e3fd0c3d8b77b4588cb26a7
Edward Delaporte <edward@delaporte.us>
2014-06-04 12:06:13 -0500

    Getting fancy, maybe overdoing it.

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -1,4 +1,5 @@
 function ssh-copy-id {
-	cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+	cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys; chmod 700 ~/.ssh && chmod 600 ~/.ssh/*"
 	cat ~/.ssh/id_rsa.pub | ssh $1 "cat >> ~/id_rsa.pub"
 }
+
